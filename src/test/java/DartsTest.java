1c7dc3a265b185a06cd0b4176f82a6ec3565844b
=raz.manciu <=raz.manciu@gmail.com>
2019-02-02 21:00:27 +0200

    Darts git remote add origin https://github.com/RazvanManciu/Darts.git git push -u origin master git commit -m Darts

---
+++ src/test/java/DartsTest.java
@@ -0,0 +1,14 @@
+// DARTS FINAL
+
+import org.junit.Before;
+
+public class DartsTest {
+    //DartsTest class in MASTER
+    //DARTS_Initial
+    private Darts dartBoard;
+
+    @Before
+    public void setUp() {
+        dartBoard = new Darts();
+    }
+}
